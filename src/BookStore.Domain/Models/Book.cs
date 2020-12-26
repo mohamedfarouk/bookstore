50651ae1e816aecc0e8d40d17706ba9bf6baf182
Mohamed Farouk <mohamed.ahmed@tpay.me>
2020-12-26 23:48:27 +0200

    add model validations via data annotations

--- src/BookStore.Domain/Models/Book.cs
+++ src/BookStore.Domain/Models/Book.cs
@@ -1,12 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace BookStore.Domain.Models
 {
     public class Book
     {
         public int ID { get; set; }
+
+        [Required]
         public string Title { get; set; }
+        
         public string Description { get; set; }
+
+        [Required]
         public string Author { get; set; }
+        
         public string CoverImageUri { get; set; }
+
+        [Required]
+        [Range(0, 1000)]
         public decimal Price { get; set; }
 
     }
